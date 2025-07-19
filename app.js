7a590ede300c4b6c84e85ab850f915c55856c5d1
Ramón Báez <ramonnoelbaezlopez.15@gmail.com>
2025-07-19 15:59:32 -0700

    Tercer commit: Cargar los partido automáticamente desde JS y agregar JS en HTML

--- app.js
+++ app.js
@@ -1,15 +1,58 @@
-async function cargarDatos() {
-    const res = await fetch("data.json");
-    const datos = await res.json();
+const ligas = [
+    {
+        nombre: "Liga MX",
+        partidos: [
+            { equipo1: "América", equipo2: "Guadalajarra" },
+            { equipo1: "Tigres", equipo2: "Monterrey" }
+        ]
+    },
+    {
+        nombre: "Champions League",
+        partidos: [
+            { equipo1: "Real Madrid", equipo2: "Machester City" },
+            { equipo1: "PSG", equipo2: "Barcelona" }
+        ]
+    },
+    {
+        nombre: "League Cup",
+        partidos: [
+            { equipo1: "Mazatlán", equipo2: "Inter Miami" }
+        ]
+    }
+];
 
-    const container = document.getElementById("partidos-container");
+//Función para renderizar en el HTML
+function renderizarLigas() {
+    const main = document.querySelector("main");
+    main.innerHTML = ""; // Limpiar contenido anterior
 
-    datos.partidos.forEach((partidos) => {
-        const div = document.createElement("div");
-        div.className = "partido";
-        div.innerHTML = `<strong>${partidos.local}</strong> vs <strong>${partido.visitante}</strong><br />
-        Resultado real: ${partido.resultado}`; 
-        container.appendChild(div);
+    ligas.forEach((liga) => {
+        const seccion = document.createElement("section");
+        seccion.classList.add("liga");
+
+        const titulo = document.createElement("h2");
+        titulo.textContent = liga.nombre;
+        seccion.appendChild(titulo);
+
+        liga.partidos.forEach((partido) => {
+            const divPartido = document.createElement("div");
+            divPartido.classList.add("partido");
+
+            const texto = document.createElement("span");
+            texto.textContent = `${partido.equipo1} vs ${partido.equipo2}`;
+
+            const input = document.createElement("input");
+            input.type = "text";
+            input.placeholder = "Tu pronostico";
+
+            divPartido.appendChild(texto);
+            divPartido.appendChild(input);
+            seccion.appendChild(divPartido);
+        });
+
+        main.appendChild(seccion);
     });
 }
-cargarDatos();
+
+// Ejecutar al cargar la pagina
+document.addEventListener("DOMContentLoaded", renderizarLigas);
